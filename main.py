a494f36b665daeb0408315b5138576e47a2f42f6
NIIIVI <163434216+NIIIVI@users.noreply.github.com>
2024-05-08 10:50:05 -0300

    Python idade

---
+++ main.py
@@ -0,0 +1,9 @@
+idade=int(input("Insira sua idade ai anciao"))
+if idade <=100:
+    print("voce é um anciao pequeno os cara KAKAKAKAKAKAKAKAKAKAKAKAKA")
+elif idade <=200:
+    print("voce é um ancio medio os cara LKAKAKAKKAKAKAKAKA")
+elif 300<= idade <=500:
+    ("ta ficando velho em mano")
+else: 
+    print("MORRE NUNCA NÃO É?")
